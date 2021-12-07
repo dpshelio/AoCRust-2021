aa3ea5298875b75141865be7a742a8e9f34e7450
David Perez-Suarez <d.perez-suarez@ucl.ac.uk>
2021-12-07 19:33:15 +0000

    Adds a simple argument parser to load the inputs from the cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::env;
+
+
+fn help() {
+    println!("usage:
+rustaoc <puzzle_number> <input>
+    runs the puzzle for the given input.
+
+");
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to AoC solutions with RUST! (cc 2021)");
+
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        // no arguments - only the name of the program
+        2 => {
+            println!("Probably you should give us an extra file!");
+        },
+        3 => {
+            let puzzle = &args[1];
+            let input = &args[2];
+            println!("I'm going to run {} with {} as input", puzzle, input);
+            }
+        },
+        1 |  _ => {
+            help();
+        },
+
+    }
 }
